46685c722098dc594891071525a7865232634795
Sebastian Alvarez Zapata <alvarez17.sebastian@gmail.com>
2018-05-15 23:11:12 -0500

    Other commit comment

--- app/src/main/java/com/practice/sebas/practicademapas/MainActivity.java
+++ app/src/main/java/com/practice/sebas/practicademapas/MainActivity.java
@@ -10,5 +10,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //Practicando integracion con GIT.
+        //Other comment.
+        //Develop branch
     }
 }
