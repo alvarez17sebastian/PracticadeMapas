c9b58cbb574ed829d5b74fff98baca025a130227
Sebastian Alvarez Zapata <alvarez17.sebastian@gmail.com>
2018-05-18 16:14:28 -0500

    Other commit comment

--- app/src/main/java/com/practice/sebas/practicademapas/MainActivity.java
+++ app/src/main/java/com/practice/sebas/practicademapas/MainActivity.java
@@ -1,16 +1,60 @@
 package com.practice.sebas.practicademapas;
 
+import android.Manifest;
+import android.app.FragmentTransaction;
+import android.content.pm.PackageManager;
+import android.support.constraint.ConstraintLayout;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
+public class MainActivity extends FragmentActivity implements OnMapReadyCallback {
+    private  GoogleMap googleMap;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //Practicando integracion con GIT.
-        //Other comment.
-        //Develop branch
+
+        /*MapFragment mMapFragment;
+        mMapFragment = MapFragment.newInstance();
+        FragmentTransaction fragmentTransaction =
+                getFragmentManager().beginTransaction();
+        fragmentTransaction.add(R.id.constraintContainer, mMapFragment);
+        fragmentTransaction.commit();
+        mMapFragment.getMapAsync(this);*/
+
+        SupportMapFragment mapFragment =
+                (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+    private void enableMyLocation() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            // Permission to access the location is missing.
+            PermissionUtils.requestPermission(this, LOCATION_PERMISSION_REQUEST_CODE,
+                    Manifest.permission.ACCESS_FINE_LOCATION, true);
+        } else if (mMap != null) {
+            // Access to the location has been granted to the app.
+            mMap.setMyLocationEnabled(true);
+        }
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        this.googleMap = googleMap;
+        LatLng gta = new LatLng(6.3783433,-75.4582937);
+        googleMap.addMarker(new MarkerOptions().position(gta).title("Marker"));
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(gta));
+        googleMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
+
     }
 }
